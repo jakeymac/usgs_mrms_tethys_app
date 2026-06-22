ab4018e0cb059aab0a6378ca7648135c706e8464
Jacob Johnson <jakeymacaroni@gmail.com>
2026-06-22 11:12:33 -0600

    Updated popup button text in basin map

--- tethysapp-usgs_mrms/tethysapp/usgs_mrms/public/js/basin_map.js
+++ tethysapp-usgs_mrms/tethysapp/usgs_mrms/public/js/basin_map.js
@@ -3,7 +3,7 @@ function customPropertiesGenerator(feature, layer) {
     return "<table class='table table-striped table-bordered table-condensed'>" +
         "<tr><th>Gage ID</th><td>" + properties.gage_id + "</td></tr>" +
         "</table>" +
-        "<button class='btn btn-primary' onclick='window.location.href = \"" + properties.gage_id + "\"'>Go to page</button>";
+        "<button class='btn btn-primary' onclick='window.location.href = \"" + properties.gage_id + "\"'>Go to analysis</button>";
 }
 
 $(function() { //wait for page to load
